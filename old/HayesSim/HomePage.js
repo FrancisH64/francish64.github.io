99fc770dd6cc2b0d9d58898b6ee49e0e4616d88c
FrancisH64 <hayesf3@gmail.com>
2022-05-11 08:39:27 -0400

    Change

---
+++ old/HayesSim/HomePage.js
@@ -0,0 +1,32 @@
+window.onscroll = function() {OnScroll()};
+
+var header = document.getElementById("hd");
+var header2 = document.getElementById("hd2");
+var footer = document.getElementById("ft")
+var sticky = header.offsetTop;
+var sticky2 = header2.offsetTop
+
+function OnScroll() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    if (window.scrollY > sticky && window.scrollY < sticky2) {
+        header.classList.add("sticky");
+        header.classList.add("shadow");
+    } else {
+        header.classList.remove("sticky");
+        header.classList.remove("shadow");
+    }
+    if (window.pageYOffset > sticky2) {
+        header2.classList.add("sticky");
+        header2.classList.add("shadow");
+    } else {
+        header2.classList.remove("sticky");
+        header2.classList.remove("shadow");
+    }
+    if (scrolled == 100) {
+        footer.classList.remove("bottom-shadow");
+    } else {
+        footer.classList.add("bottom-shadow");
+    }
+}
